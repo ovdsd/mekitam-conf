f3ca95250907c50d25303c55d562b0b77b716619
ovdsd <81808543+LeonClausius@users.noreply.github.com>
2023-10-03 23:25:43 +0700

    capek lanjutin

--- index.js
+++ index.js
@@ -1,36 +1,41 @@
 const express = require('express');
-const app = express(); 
-const server = require('http').Server(app); 
-const io = require('socket.io')(server); 
-const { ExpressPeerServer } = require('peer'); 
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const { ExpressPeerServer } = require('peer');
 const PORT = process.env.PORT || 8000;
 
 const peerServer = ExpressPeerServer(server, {
     debug: true
 });
 
-
 app.use(express.static('./assets'));
-app.set('view engine', 'ejs'); 
-app.set('views', './views'); 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+
 app.use('/', require('./routes/index'));
 
-io.on('connection', socket => {
+io.on('connection', (socket) => {
     socket.on('join-room', (roomId, userId, userName) => {
         socket.join(roomId);
         socket.broadcast.to(roomId).emit('user-connected', userId, userName);
         socket.on('send-message', (inputMsg, userName) => {
             io.to(roomId).emit('recieve-message', inputMsg, userName);
-        })
+        });
         socket.on('disconnect', () => {
             socket.broadcast.to(roomId).emit('user-disconnected', userId, userName);
-        })
+        });
     });
 });
 
-server.listen(PORT, function (err) {
+server.listen(PORT, (err) => {
     if (err) {
-        console.log(`Error :: ${err} occured while starting the server in index.js!`);
+        console.log(`Error :: ${err} occurred while starting the server in index.js!`);
     }
     console.log(`Server is up and running on port ${PORT}`);
 });
